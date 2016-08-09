42a8110dde6c3d0d23134abfaceb476877880b3f
BiaoWu <biaoliu_2007@163.com>
2016-08-09 14:31:24 +0800

    initial project

---
+++ BiOS/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BiOS
+//
+//  Created by BiaoWu on 16/8/9.
+//  Copyright © 2016年 BiaoWu. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
